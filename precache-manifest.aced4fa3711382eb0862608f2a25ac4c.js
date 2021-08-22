81eb40d8ed29db9c417edf3c431163fafd1a1801
Shanuktrml <89344371+Shanuktrml@users.noreply.github.com>
2021-08-22 18:51:09 +0530

    Add files via upload

---
+++ precache-manifest.aced4fa3711382eb0862608f2a25ac4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0adb19ab748f9d4b92eb",
+    "url": "./static/css/main.f819cfbd.chunk.css"
+  },
+  {
+    "revision": "0adb19ab748f9d4b92eb",
+    "url": "./static/js/main.0adb19ab.chunk.js"
+  },
+  {
+    "revision": "b13218e949d73dc09388",
+    "url": "./static/css/1.75ac6f77.chunk.css"
+  },
+  {
+    "revision": "b13218e949d73dc09388",
+    "url": "./static/js/1.b13218e9.chunk.js"
+  },
+  {
+    "revision": "0930cedb48f5259c0bdd",
+    "url": "./static/js/runtime~main.0930cedb.js"
+  },
+  {
+    "revision": "b0b879a5de7a3837aa9044ebcd0d4f09",
+    "url": "./static/media/girlGraphic.b0b879a5.png"
+  },
+  {
+    "revision": "a0f896955210accd7f0f0b9ed6dfa180",
+    "url": "./static/media/hijab.a0f89695.jpg"
+  },
+  {
+    "revision": "9bb047923a206e76c3bf220431e922b5",
+    "url": "./index.html"
+  }
+];
